6c88ce71d462b55ae9ff0ba5bea25bc9cfd933a3
Peter Damgaard Marschall <15450@student.eadania.dk>
2023-03-03 14:29:03 +0100

    Add project files.

---
+++ Portfolio_MauiNewsfeed/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Portfolio_MauiNewsfeed;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ Portfolio_MauiNewsfeed/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.ServiceModel.Syndication;
+using System.Xml;
+
+namespace Portfolio_MauiNewsfeed;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+
+        
+    }
+}
