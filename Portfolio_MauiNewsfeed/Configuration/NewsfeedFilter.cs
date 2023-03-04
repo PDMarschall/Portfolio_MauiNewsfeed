213c8f5b7afefd6008493033ca093c731361f001
Peter Damgaard Marschall <15450@student.eadania.dk>
2023-03-04 18:57:13 +0100

    can click on a filter and choose it

--- Portfolio_MauiNewsfeed/Configuration/NewsfeedFilter.cs
+++ Portfolio_MauiNewsfeed/Configuration/NewsfeedFilter.cs
@@ -11,5 +11,19 @@ namespace Portfolio_MauiNewsfeed.Configuration
         public string Title { get; set; }        
         public List<string> UserWhitelist { get; set; } = new List<string> { };
         public List<string> UserBlacklist { get; set; } = new List<string> { };
+
+        public void Clear()
+        {
+            Title= string.Empty;
+            UserWhitelist.Clear();
+            UserBlacklist.Clear();
+        }
+
+        public void CopyFilterValues(NewsfeedFilter filter)
+        {
+            this.Title = filter.Title;
+            this.UserWhitelist = filter.UserWhitelist;
+            this.UserBlacklist = filter.UserBlacklist;
+        }
     }
 }
