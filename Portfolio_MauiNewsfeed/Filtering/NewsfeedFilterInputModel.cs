5e1a7a2331fcf4f50716e98d8916d838bdc204b3
Peter Damgaard Marschall <15450@student.eadania.dk>
2023-03-08 15:27:03 +0100

    added and tested attribute for requiring disjunctive properties

--- Portfolio_MauiNewsfeed/Filtering/NewsfeedFilterInputModel.cs
+++ Portfolio_MauiNewsfeed/Filtering/NewsfeedFilterInputModel.cs
@@ -9,15 +9,22 @@ namespace Portfolio_MauiNewsfeed.Filtering
 {
     public class NewsfeedFilterInputModel
     {
-        private readonly char[] _separators = { ' ', ',', '.', ';', ':', '?', '!' };        
-
+        private readonly char[] _separators = { ' ', ',', '.', ';', ':', '?', '!' };
+                
         [Required(ErrorMessage = "Please input a title"), MinLength(1)]
         public string Title { get; set; } = string.Empty;
 
-        [RegularExpression(@"^\b[a-zæøåA-ZÆØÅ]+\b(?:[ ,.;:?!]\s*[a-zæøåA-ZÆØÅ]+\b){0,2}$", ErrorMessage = "1 - 3 ord.")]
+        [RegularExpression(@"^\b[a-zæøåA-ZÆØÅ]+\b(?:[ ,.;:?!]\s*[a-zæøåA-ZÆØÅ]+\b){0,2}$",
+            ErrorMessage = "Vælg 1 - 3 ord."),
+            RequireDisjunctive("Whitelist", "Blacklist",            
+            ErrorMessage = "Please input at least one word in either list.")]
+
         public string Whitelist { get; set; } = string.Empty;
 
-        [RegularExpression(@"^\b[a-zæøåA-ZÆØÅ]+\b(?:[ ,.;:?!]\s*[a-zæøåA-ZÆØÅ]+\b){0,2}$", ErrorMessage = "1 - 3 ord.")]
+        [RegularExpression(@"^\b[a-zæøåA-ZÆØÅ]+\b(?:[ ,.;:?!]\s*[a-zæøåA-ZÆØÅ]+\b){0,2}$",
+            ErrorMessage = "Vælg 1 - 3 ord."),
+            RequireDisjunctive("Whitelist", "Blacklist",
+            ErrorMessage = "Please input at least one word in either list.")]
         public string Blacklist { get; set; } = string.Empty;
 
         public NewsfeedFilter ConvertInputModel()
---
+++ Portfolio_MauiNewsfeed/Filtering/RequireDisjunctive.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio_MauiNewsfeed.Filtering
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public sealed class RequireDisjunctive : ValidationAttribute
+    {
+        private string[] _disjunctiveProperties;
+
+        public RequireDisjunctive(params string[] propertyNames)
+        {
+            _disjunctiveProperties = propertyNames;
+        }        
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            return ValidateDisjunctiveProperties(value, validationContext) ? ValidationResult.Success : new ValidationResult(ErrorMessage);
+        }
+
+        private bool ValidateDisjunctiveProperties(object value, ValidationContext validationContext)
+        {            
+            List<object> propertyValues = new List<object>();
+
+            foreach (string propertyName in _disjunctiveProperties)
+            {
+                propertyValues.Add(validationContext.ObjectType.GetProperty($"{propertyName}").GetValue(validationContext.ObjectInstance, null));
+            }
+
+            List<bool> results= new List<bool>();
+
+            foreach (object propertyValue in propertyValues)
+            {
+                if (propertyValue == null || (string)propertyValue == string.Empty)
+                {
+                    results.Add(false);
+                }
+                else
+                {
+                    results.Add(true);
+                }                
+            }
+
+            return results.Any(x => x == true);
+        }
+    }
+}
