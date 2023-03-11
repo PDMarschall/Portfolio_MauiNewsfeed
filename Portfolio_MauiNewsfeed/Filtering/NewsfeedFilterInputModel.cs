25c3d7b73bd70acd8c33f823b8d00aa23720ac67
Peter Damgaard Marschall <15450@student.eadania.dk>
2023-03-11 19:30:05 +0100

    allowed for whitespaces after first word added to new filter, fixed two typos

--- Portfolio_MauiNewsfeed/Filtering/NewsfeedFilterInputModel.cs
+++ Portfolio_MauiNewsfeed/Filtering/NewsfeedFilterInputModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Portfolio_MauiNewsfeed.Filtering
@@ -12,21 +13,21 @@ namespace Portfolio_MauiNewsfeed.Filtering
     {
         private readonly char[] _separators = { ' ', ',', '.', ';', ':', '?', '!' };
         private const string WhitelistName = "Whitelist";
-        private const string BlacklistName = "Blacklist";
+        private const string BlacklistName = "Blacklist";        
         private const string RegexError = "Mindst 1 ord og maksimalt 3 ord.";
         private const string DisjunctionError = "Vælg mindst 1 ord til enten whitelist eller blacklist.";
 
         [Required(ErrorMessage = "Vælg en titel."), MinLength(1)]
         public string Title { get; set; } = string.Empty;
 
-        [RegularExpression(@"^\b[a-zæøåA-ZÆØÅ]+\b(?:[ ,.;:?!]\s*[a-zæøåA-ZÆØÅ]+\b){0,2}$",
+        [RegularExpression(@"^\b[a-zæøåA-ZÆØÅ]+\b(?:[ ,.;:?!]\s*[a-zæøåA-ZÆØÅ]+\b){0,2}\s*$",
             ErrorMessage = RegexError),
             RequiredDisjunction(new string[] { WhitelistName, BlacklistName },            
             ErrorMessage = DisjunctionError)]
 
         public string Whitelist { get; set; } = string.Empty;
 
-        [RegularExpression(@"^\b[a-zæøåA-ZÆØÅ]+\b(?:[ ,.;:?!]\s*[a-zæøåA-ZÆØÅ]+\b){0,2}$",
+        [RegularExpression(@"^\b[a-zæøåA-ZÆØÅ]+\b(?:[ ,.;:?!]\s*[a-zæøåA-ZÆØÅ]+\b){0,2}\s*$",
             ErrorMessage = RegexError),
             RequiredDisjunction(new string[] { WhitelistName, BlacklistName },
             ErrorMessage = DisjunctionError)]
